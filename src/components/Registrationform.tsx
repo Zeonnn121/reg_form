b4496c9f4e25ed3c7d326a822f6307afbb88fbab
Zeonnn121 <zeond1816@gmail.com>
2025-06-04 17:58:39 +0530

    frontend fetch

--- src/components/Registrationform.tsx
+++ src/components/Registrationform.tsx
@@ -104,7 +104,7 @@ const handleSubmit = async (e: React.FormEvent) => {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch('http://localhost:5000/api/register', {
+      const response = await fetch('https://reg-form-tvho.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
